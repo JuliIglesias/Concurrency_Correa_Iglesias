352c008b7454911732a088ffc22f24c448e50ad8
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-03-25 01:41:02 -0300

    read files

--- tp3/grep_engine/src/lib.rs
+++ tp3/grep_engine/src/lib.rs
@@ -1,8 +1,15 @@
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::thread;
+use std::{io, thread};
 
-pub fn read_file() {
-
+pub fn read_file(file_path: &str) -> Result<Vec<String>, io::Error> {
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+    let lines = reader
+        .lines()
+        .map(|line| line.unwrap())
+        .filter(|line| !line.trim().is_empty())
+        .collect();
+    Ok(lines)
 }
