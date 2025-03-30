2108632ccb8f639fe3f5cda5eaa4f1489e21f620
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-03-30 20:31:24 -0300

    grep rename package

--- grep/grep_engine/tests/read_file.rs
+++ grep/grep_engine/tests/read_file.rs
@@ -3,7 +3,7 @@ mod tests{
     use grep_engine::read_file;
     #[test]
     fn read_file_with_one_line_test_001() {
-        let file = read_file("C:/Users/Julieta/projects/concurrency/Concurrency_Correa_Iglesias/tp3/grep_engine/tests/test_001.txt").unwrap();
+        let file = read_file("C:/Users/Julieta/projects/concurrency/Concurrency_Correa_Iglesias/grep/grep_engine/tests/test_001.txt").unwrap();
         let mut expected: Vec<String> = Vec::new();
         expected.push("Hello, World!".to_string());
 
@@ -12,7 +12,7 @@ mod tests{
 
     #[test]
     fn read_file_with_two_lines_test_002(){
-        let file = read_file("C:/Users/Julieta/projects/concurrency/Concurrency_Correa_Iglesias/tp3/grep_engine/tests/test_002.txt").unwrap();
+        let file = read_file("C:/Users/Julieta/projects/concurrency/Concurrency_Correa_Iglesias/grep/grep_engine/tests/test_002.txt").unwrap();
         let mut expected: Vec<String> = Vec::new();
         expected.push("Hello, World!".to_string());
         expected.push("Goodbye, World!".to_string());
