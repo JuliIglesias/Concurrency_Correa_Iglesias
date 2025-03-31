4c297cfd92adfb02c93cf143becbefb81fc44339
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-03-30 21:47:22 -0300

    cargo and stuff

---
+++ tp3/src/main.rs
@@ -0,0 +1,17 @@
+use std::net::TcpListener;
+use std::thread;
+use lib::handle_connection;
+use tp3::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+}
