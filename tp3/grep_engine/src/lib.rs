b1ee74809fe91bd2e6fd9140c101ec64735eead9
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-03-30 23:30:17 -0300

    Reworked concurrent chunk search

--- tp3/grep_engine/src/lib.rs
+++ tp3/grep_engine/src/lib.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Result;
-use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Instant;
 
@@ -14,7 +13,7 @@ pub enum SearchMode {
 }
 
 pub fn search_word(pattern: &str, paths_files: Vec<String>, mode: SearchMode) -> (Result<Vec<String>>, f64) {
-    let mut results: Result<Vec<String>>;
+    let results: Result<Vec<String>>;
 
     let now = Instant::now();
     match mode {
@@ -33,11 +32,11 @@ fn search_word_sequential(pattern: &str, paths_files: Vec<String>) -> Result<Vec
 
     for path_file in paths_files {
         let file_lines: Vec<String> = read_file(path_file.as_str())?;
-        let filtered_lines: Vec<String> = file_lines.filter(|line| line.contains(pattern)).collect::<Vec<String>>();
+        let filtered_lines: Vec<String> = file_lines.into_iter().filter(|line| line.contains(pattern)).collect::<Vec<String>>();
         result.push(filtered_lines);
     }
 
-    let flatted_result = result.iter().flatten().collect::<Vec<String>>();
+    let flatted_result = result.into_iter().flatten().collect::<Vec<String>>();
     Ok(flatted_result)
 }
 
@@ -67,7 +66,7 @@ fn search_word_concurrent_chunk(pattern: &str, paths_files: Vec<String>) -> Resu
                 thread::scope(|chunk_scope| {
                     let chunks_threads_results: Vec<Vec<String>> = chunk_lines.map(|chunk_line| {
                         chunk_scope.spawn(move || {
-                            chunk_line.to_vec().iter().filter(|line| line.contains(pattern)).collect::<Vec<String>>()
+                            chunk_line.to_vec().into_iter().filter(|line| line.contains(pattern)).collect::<Vec<String>>()
                         })
                     })
                         .map(|chunk_thread_result| {
@@ -86,30 +85,6 @@ fn search_word_concurrent_chunk(pattern: &str, paths_files: Vec<String>) -> Resu
 
         Ok(threads_results.into_iter().flatten().collect::<Vec<String>>())
     })
-
-    // let result = Arc::new(Mutex::new(Vec::new()));
-    //
-    // thread::scope(|s| {
-    //     for path_file in paths_files {
-    //         let result = Arc::clone(&result);
-    //         s.spawn(move || {
-    //             if let Ok(file_lines) = read_file(path_file.as_str()) {
-    //                 let chunks = file_lines.chunks(4);
-    //                 for chunk in chunks {
-    //                     let result = Arc::clone(&result);
-    //                     let chunk: Vec<String> = chunk.to_vec();
-    //                     s.spawn(move || {
-    //                         let mut result = result.lock().unwrap();
-    //                         push_result(pattern, &mut result, chunk);
-    //                     });
-    //                 }
-    //             }
-    //         });
-    //     }
-    // });
-    //
-    // let result = Arc::try_unwrap(result).unwrap().into_inner().unwrap();
-    // Ok(result)
 }
 
 pub fn read_file(file_path: &str) -> Result<Vec<String>> {
