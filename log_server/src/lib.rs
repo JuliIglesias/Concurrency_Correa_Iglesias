b8ab87d22c22c3774a2bab041cd2f494f8e79fb9
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2025-04-13 22:55:05 -0300

    log_server library created

---
+++ log_server/src/lib.rs
@@ -0,0 +1,17 @@
+mod routes_controller;
+
+use std::io::Read;
+use std::net::TcpStream;
+use routes_controller::handle_request;
+
+pub fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    handle_request(&mut buffer, &stream);
+
+    // let request = String::from_utf8_lossy(&buffer);
+    // println!("{}", request);
+}
+
+
