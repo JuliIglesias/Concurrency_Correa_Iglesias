408bc076f1718f9fd65b439c97e3d14d4d9d3b15
JuliIglesias <jiglesias@mail.austral.edu.ar>
2025-04-15 09:52:25 -0300

    threadPool in tp4

--- tp4/src/main.rs
+++ tp4/src/main.rs
@@ -1,10 +1,23 @@
 use std::net::TcpListener;
+use std::sync::{Arc, Mutex};
 use log_server::handle_connection;
+use thread_pool_lib::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // Crear un ThreadPool con un tamaño máximo de 4
+    let pool = ThreadPool::new(4);
+
+    // Datos compartidos para estadísticas
+    let stats = Arc::new(Mutex::new(log_server::Stats::new()));
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        let stats = Arc::clone(&stats);
+
+        pool.execute(move || {
+            handle_connection(stream, stats);
+        })
     }
 }
